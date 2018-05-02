929b514729e7ee2b4e67f33d0b792bf526b1d0f6
Pierry A. Pereira <pierryangelo@gmail.com>
2018-05-02 14:00:23 -0300

    json for /user/new ; /users ; /user/id are working

--- challenges/challenge01/app.py
+++ challenges/challenge01/app.py
@@ -1,9 +1,9 @@
 import os
 
 from flask import Flask
-from flask import render_template
 from flask import request
 from flask import jsonify
+from flask_accept import accept
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -15,6 +15,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
     name = db.Column(db.String(200), unique=False, nullable=False, primary_key=False)
@@ -25,32 +26,44 @@ class User(db.Model):
 
     def serialize(self):
         return {
-            'id' : self.id,
-            'name' : self.name,
-            'email' : self.email
+            'id': self.id,
+            'name': self.name,
+            'email': self.email
         }
 
-@app.route('/user/new', methods=["GET", "POST"])
+
+@app.route('/user/new', methods=["POST"])
+@accept('application/json')
 def new_user():
-    name = request.method.get('name')
-    email = request.method.get('email')
+    content = request.get_json()
+    name = content['name']
+    email = content['email']
 
     if request.method == 'POST' and name and email:
         u = User(name=name, email=email)
         db.session.add(u)
         db.session.commit()
+        return jsonify(sucess=True), 201
 
-    return render_template("new_user.html")
+    return jsonify(sucess=False), 400
 
-@app.route('/user/<int:id>', methods=["GET"])
+
+@app.route('/user/<int:user_id>', methods=["GET"])
+# @accept('application/json')
 def user_profile(user_id):
     u = User.query.get(user_id)
     if u:
         return jsonify(id=u.id, name=u.name, email=u.email)
 
-    return jsonify(sucess=False), 400
+    return jsonify(sucess=False), 404 # or 400
+
 
 @app.route('/users', methods=["GET"])
+# @accept('application/json')
 def list_users():
     users = User.query.all()
-    return jsonify(users_list = [u.serialize() for u in users])
+    return jsonify(users_list=[u.serialize() for u in users])
+
+
+if __name__ == "__main__":
+    app.run()
--- challenges/challenge01/challenge01.py
+++ challenges/challenge01/challenge01.py
@@ -1,12 +1,20 @@
 import requests
 
-# ASCII API
-text = "That's all folks!"
-font = "brite"
+# # ASCII API
+# text = "That's all folks!"
+# font = "brite"
+#
+# # text with default font
+# r = requests.get(f'http://artii.herokuapp.com/make?text={text}')
+# print(r.text)
+#
+# # text with custom font
+# r = requests.get(f'http://artii.herokuapp.com/make?text={text}&font={font}')
 
-# text with default font
-r = requests.get(f'http://artii.herokuapp.com/make?text={text}')
+
+name = 'Mr. Bean'
+email = 'bean@gmail.com'
+
+r = requests.post('http://localhost:5000/user/new', data={'name': name, 'email': email})
+print(r.status_code)
 print(r.text)
-
-# text with custom font
-r = requests.get(f'http://artii.herokuapp.com/make?text={text}&font={font}')
