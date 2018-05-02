a428b8b48023f625086412c3b762b8d604c83fe6
Pierry A. Pereira <pierryangelo@gmail.com>
2018-05-02 15:50:42 -0300

    examples done

--- challenges/challenge01/challenge01.py
+++ challenges/challenge01/challenge01.py
@@ -1,5 +1,7 @@
-import requests
+import requests, json
 
+
+# Exemplo 01
 # # ASCII API
 # text = "That's all folks!"
 # font = "brite"
@@ -12,9 +14,42 @@ import requests
 # r = requests.get(f'http://artii.herokuapp.com/make?text={text}&font={font}')
 
 
-name = 'Joao Paulo II'
-email = 'paulo2@gmail.com'
+# Exemplo 02
 
-r = requests.post('http://localhost:5000/user/new', data={'name': name, 'email': email})
+bin = 'k5hZDXsc'
+base_url = f'http://postb.in/api/bin/'
+
+# POST
+r1 = requests.post(f'http://postb.in/api/bin')
+print(r1.status_code)
+print(r1.text)
+
+# GET
+json_data = json.loads(r1.text)
+r2 = requests.get(f'{base_url}{json_data["binId"]}')
+print(r2.status_code)
+print(r2.text)
+
+
+# # CREATING A NEW USER
+# name = 'Joao Paulo II'
+# email = 'paulo2@gmail.com'
+#
+# r = requests.post('http://localhost:5000/user/new', data={'name': name, 'email': email})
+# print(r.status_code)
+# print(r.text)
+
+
+# # AN SPECIFIC USER BY ID
+# id = '1'
+# r = requests.get(f'http://localhost:5000/user/{id}')
+# print(r.status_code)
+# print(r.text)
+
+# ALL USERS
+r = requests.get(f'http://localhost:5000/users')
 print(r.status_code)
 print(r.text)
+
+
+
