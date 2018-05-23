b9eccb6d26fbc9f056f591ec160052387e0c5106
Pierry A. Pereira <pierryangelo@gmail.com>
2018-05-23 11:16:33 -0300

    script to populate database

---
+++ challenges/challenge02/convert_json.py
@@ -0,0 +1,73 @@
+import json
+from sqlalchemy import create_engine
+from sqlalchemy import ForeignKey, Column, Integer, String
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.types import Text
+
+engine = create_engine('postgresql://pierry:wd45$sds@localhost/blog')
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+
+
+class Address(Base):
+    __tablename__ = 'addresses'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    street = Column(String)
+    suite = Column(String)
+    city = Column(String)
+    zipcode = Column(String)
+
+    #user = relationship('User', back_ref="addresses")
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    title = Column(String)
+    body = Column(Text)
+
+    #user = relationship('User', back_populates="posts")
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    name = Column(String)
+    email = Column(String)
+    body = Column(Text)
+
+    #post = relationship('Post', back_populates='comments')
+
+# creates the schema for all entities
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# loads json data
+f = open('data.json')
+data = json.load(f)
+
+# POPULATING USERS TABLE
+# users = data['users'][1:]
+#
+# for user in users:
+#     u = User(id=user['id'], name=user['name'], email=user['email'])
+#     session.add(u)
+#     session.commit()
+
+# POPULATING USERS TABLE
+# users = data['users'][1:]
+#
+# for user in users:
+#     u = User(id=user['id'], name=user['name'], email=user['email'])
+#     session.add(u)
+#     session.commit()
+
