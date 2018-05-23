2c4e2957c985c7afd03a4d2dd95b67d7aae3f26a
Pierry A. Pereira <pierryangelo@gmail.com>
2018-05-23 12:17:43 -0300

    populates it all

--- challenges/challenge02/convert_json.py
+++ challenges/challenge02/convert_json.py
@@ -58,33 +58,44 @@ session = Session()
 f = open('data.json')
 data = json.load(f)
 
-## POPULATING USERS TABLE
-# users = data['users']
-#
-# for user in users:
-#     u = User(id=user['id'], name=user['name'], email=user['email'])
-#     session.add(u)
-#     session.commit()
+# POPULATING USERS TABLE
+users = data['users']
+
+for user in users:
+    u = User(id=user['id'], name=user['name'], email=user['email'])
+    session.add(u)
+    session.commit()
 
 
-## POPULATING ADDRESSES TABLE
-# users = data['users']
-#
-# for user in users:
-#     u_adress = user['address']
-#     a = Address(id=user['id'], user_id=user['id'], street=u_adress['street'],
-#                 suite=u_adress['suite'], city=u_adress['city'],
-#                 zipcode=u_adress['zipcode'])
-#     session.add(a)
-#     session.commit()
+# POPULATING ADDRESSES TABLE
+users = data['users']
+
+for user in users:
+    u_adress = user['address']
+    a = Address(id=user['id'], user_id=user['id'], street=u_adress['street'],
+                suite=u_adress['suite'], city=u_adress['city'],
+                zipcode=u_adress['zipcode'])
+    session.add(a)
+    session.commit()
 
 
-## POPULATING POSTS TABLE
-# posts = data['posts']
-#
-# for post in posts:
-#     p = Post(id=post['id'], user_id=post['userId'],
-#              title=post['title'], body=post['body'])
-#
-#     session.add(p)
-#     session.commit()
+# POPULATING POSTS TABLE
+posts = data['posts']
+
+for post in posts:
+    p = Post(id=post['id'], user_id=post['userId'],
+             title=post['title'], body=post['body'])
+
+    session.add(p)
+    session.commit()
+
+# POPULATING COMMENTS TABLE
+comments = data['comments']
+
+for comment in comments:
+    c = Comment(id=comment['id'], post_id=comment['postId'],
+                name=comment['name'], email=comment['email'],
+                body=comment['body'])
+
+    session.add(c)
+    session.commit()
