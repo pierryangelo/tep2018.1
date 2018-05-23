86c82e713fd4eefaf0d2d063bde8301d3c11a9f1
Pierry A. Pereira <pierryangelo@gmail.com>
2018-05-22 22:06:10 -0300

    second model

--- challenges/challenge02/blog/core/models.py
+++ challenges/challenge02/blog/core/models.py
@@ -5,6 +5,12 @@ class User(models.Model):
     name = models.CharField(max_length=200, blank=False)
     email = models.EmailField(blank=False)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
 
 class Address(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,
@@ -14,12 +20,28 @@ class Address(models.Model):
     city = models.CharField(max_length=200)
     zipcode = models.CharField(max_length=10)
 
+    def __str__(self):
+        return f'Street: {self.street},' \
+               f'Suite: {self.suite},' \
+               f'City: {self.city},' \
+               f'Zipcode: {self.zipcode} '
+
+    class Meta:
+        ordering = ('street',)
+
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              related_name='posts')
     title = models.CharField(max_length=200)
     body = models.TextField()
+
+    def __str__(self):
+        return f'Title: {self.title},' \
+               f'Body: {self.body},'
+
+    class Meta:
+        ordering = ('title',)
 
 
 class Comment(models.Model):
@@ -28,3 +50,8 @@ class Comment(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
     body = models.TextField()
+
+    def __str__(self):
+        return f'Name: {self.name},' \
+               f'E-mail: {self.email},' \
+               f'Body: {self.body}'
