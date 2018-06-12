586bf51354292e7b733c8e66bf76caa1552061c5
Pierry A. Pereira <pierryangelo@gmail.com>
2018-06-12 16:22:45 -0300

    trying first nested approach

--- challenges/challenge02/blog/core/urls.py
+++ challenges/challenge02/blog/core/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path(r'users/', views.UserList.as_view(), name='user-list'),
     path(r'user/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path(r'user/<int:pk>/posts', views.UserPosts.as_view(), name='user-posts'),
     path(r'posts/', views.PostList.as_view(), name='post-list'),
     path(r'post/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
     path(r'comments/', views.CommentList.as_view(), name='comment-list'),
--- challenges/challenge02/blog/core/views.py
+++ challenges/challenge02/blog/core/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from .serializers import UserSerializer, CommentSerializer
 from .serializers import PostSerializer, AddressSerializer
@@ -43,8 +44,13 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
-#
-# class UserSummaryList(generics.ListCreateAPIView):
-#     serializer_class = UserSummarySerializer
-#     queryset = UserSummary.objects.all()
 
+
+class UserPosts(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    lookup_url_kwarg = 'pk'
+
+    def get_queryset(self):
+        user_pk = self.kwargs.get(self.lookup_url_kwarg)
+        user_posts = Post.objects.filter(user__pk=user_pk)
+        return user_posts
