e13c39ff08cb0c84be4288410db56345c2e773a9
Pierry A. Pereira <pierryangelo@gmail.com>
2018-06-26 10:43:27 -0300

    token working

--- challenges/challenge02/blog/blog/settings.py
+++ challenges/challenge02/blog/blog/settings.py
@@ -52,9 +52,18 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': (
+         'rest_framework.throttling.AnonRateThrottle',
+         'rest_framework.throttling.UserRateThrottle',
+     ),
+     'DEFAULT_THROTTLE_RATES': {
+         'anon': '20/hour',
+         'user': '10/hour',
+         'api-token': '1/hour',
+    },
 }
 
 MIDDLEWARE = [
