36a295a56053a48cc1cfe2d274bf9025025f8fb7
Pierry A. Pereira <pierryangelo@gmail.com>
2018-07-03 18:28:14 -0300

    before model change

--- projects/primus2/core/admin.py
+++ projects/primus2/core/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth import forms
 from django.contrib.auth.admin import UserAdmin
 
 from core.models import PlanoDeEstudo, Atividade, Disciplina, Assunto, Anotacao, User
